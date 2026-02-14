e75b81863fcc46cfa157bdbdcb11baff8ef391f2
sqtwix <ivan20140767@gmail.com>
2026-02-14 20:49:01 +0300

    feat: ValueQueryService

---
+++ CsvApi/CsvApi.Application/Services/ValueQueryService.cs
@@ -0,0 +1,39 @@
+﻿using CsvApi.Application.Interfaces;
+using System;
+using CsvApi.Application.DTOs;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvApi.Application.Services
+{
+    public class ValueQueryService : IValueQueryService
+    {
+        // ValueQueryService - сервис для получения последних 10 значений из БД по имени файла.
+        private readonly IValueRepository _valueRepository;
+        public ValueQueryService(IValueRepository valueRepository)
+        {
+            _valueRepository = valueRepository;
+        }
+
+        public async Task<List<ValueDto>> GetLast10Async(string fileName)
+        {
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                throw new ArgumentException("Имя файла не может быть пустым");
+            }
+
+            var trimmedFileName = fileName.Trim();
+
+            var values = await _valueRepository.GetLast10ByFileNameAsync(trimmedFileName);
+
+            return values.Select(v => new ValueDto(
+                Date: v.Date,           
+                ExecutionTime: v.ExecutionTime,   
+                Value: v.FileValue
+            )).ToList();
+        }
+
+    }
+}
